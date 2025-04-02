8ca2934903ec4ab83dbb27f5a120a48bd6c476fe
Solhee <ellen.solhee.jin@gmail.com>
2025-04-02 11:23:56 -0700

    Implemented middle_school_student, along with relevant tests.

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,15 @@
 from .student import Student
 
 # add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation=False):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+    
+    def summary(self):
+        parent_summary = super().summary()
+        if self.gets_transportation:
+            transportation_summary = f"{self.name} needs transportation."
+        else:
+            transportation_summary = f"{self.name} does not need transportation."
+        return (f"{parent_summary}. {transportation_summary}")
